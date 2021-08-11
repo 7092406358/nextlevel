553e688c23a480417cc2efdede3c77226ed1502e
7092406358 <arunkumarsaratha@gmail.com>
2021-08-11 07:33:27 +0530

    my first push

---
+++ src/org/tube/Selen.java
@@ -0,0 +1,38 @@
+package org.tube;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Selen {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.setProperty("WebDriver.Chrome.driver","C:\\Users\\ARUNKUMAR\\esclipse new\\First\\Driver\\chromedriver.exe");
+		 WebDriver driver= new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Register.html");
+		driver.manage().window().maximize();
+		
+	WebElement s = driver.findElement(By.id("countries"));
+	
+	
+	
+Select	g=new Select(s);
+List<WebElement> options = g.getOptions();
+for(int i=0;i<options.size();i++) {
+	String text = options.get(i).getText();
+	System.out.println(text);
+}
+
+
+
+	
+
+
+	
+	}
+}
